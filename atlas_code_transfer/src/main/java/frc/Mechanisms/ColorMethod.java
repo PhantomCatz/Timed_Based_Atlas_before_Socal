b19f84fee86178da13b78bc07e224ca25e11a8da
YKS44 <79933420+YKS44@users.noreply.github.com>
2023-07-13 15:49:16 -0700

    Added description of ColorMethod.java

--- atlas_code_transfer/src/main/java/frc/Mechanisms/ColorMethod.java
+++ atlas_code_transfer/src/main/java/frc/Mechanisms/ColorMethod.java
@@ -2,6 +2,12 @@ package frc.Mechanisms;
 
 import edu.wpi.first.wpilibj.util.Color;
 
+/**
+ * This is a simple interface that can be used to store methods into a variable. 
+ * We had to create a custom one since the built-in functional interface, Consumer, does not support varargs (or the ... thing that allows multiple parameters)
+ * 
+ * Interfaces can essentially be used for two things: serve as a basis of a class or used as a collection of methods whose actions are determined on instantiation. 
+ */
 public interface ColorMethod {
      public void execute(Color... param);
 }
